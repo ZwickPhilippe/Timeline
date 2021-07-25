efffc87d8a2508112f008cefa7857dff087451d1
philippe zwick <philippe.zwick@atos.net>
2021-07-25 11:40:10 +0200

    Fixing timeline points, adding linkProp

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -8,7 +8,7 @@ function Timeline(){
     return(
         <div className="timeline">
             <TimelineContainer left={true} headline={"Titel 1"} content={"Das ist der aktuelle Wert"}></TimelineContainer>
-            <TimelineContainer left={false} headline={"Titel 2"} content={"Das ist der aktuelle Wert"}></TimelineContainer>
+            <TimelineContainer left={false} headline={"Sorting Algorithms (+Visualizer)"} content={"In algorithmics I learned about the simple and the more complex (but therefore faster) sorting algorithms. I decided to create a visualizer, that can be viewed here: "} link={"https://github.com/ZwickPhilippe/Sorting-Visualizer"} linkName={"Sorting-Visualizer"}></TimelineContainer>
             <TimelineContainer left={true} headline={"Titel 3"} content={"Das ist der aktuelle Wert"}></TimelineContainer>
             <TimelineContainer left={false} headline={"Titel 4"} content={"Das ist der aktuelle Wert"}></TimelineContainer>
 
--- src/timeline/TimelineContainer.js
+++ src/timeline/TimelineContainer.js
@@ -7,6 +7,7 @@ function TimelineContainer(props){
 
     const printProp=()=>{
         console.log(props.left)
+        console.log(props.link)
     }
 
 
@@ -14,7 +15,7 @@ function TimelineContainer(props){
             <React.Fragment>
             <div className={'timelineContainer ' + (props.left? "left" : "right")}>
                 <h2>{props.headline}</h2>
-                <p>{props.content}</p>
+                <p>{props.content}  <a href={props.link} style={{textDecoration: 'none'}}>{props.linkName}</a> <i className="fas fa-sitemap"></i></p>
             </div>
            
             </React.Fragment>
