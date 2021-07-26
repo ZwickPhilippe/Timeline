c96e59b44b6618572f5e594c7b6f578db9484244
philippe zwick <philippe.zwick@atos.net>
2021-07-26 18:46:47 +0200

    First Tries with newer version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Timeline from './timeline/Timeline';
+import logo from "./logo.svg";
+import "./App.css";
+import Timeline from "./timeline/Timeline";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 function App() {
   return (
     <div className="background">
       <Timeline></Timeline>
+      <Canvas>
+        <Suspense fallback={null}></Suspense>
+      </Canvas>
     </div>
   );
 }
